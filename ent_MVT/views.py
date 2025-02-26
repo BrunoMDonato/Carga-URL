9b81a1a53507692e084f001388a09b19d224ae5a
Bruno <102097102+BrunoMDonato@users.noreply.github.com>
2025-02-26 19:53:00 -0300

    Update views.py

--- ent_MVT/views.py
+++ ent_MVT/views.py
@@ -5,9 +5,10 @@ from ent_MVT.models import *
 def familia(request, dni, edad, nombre):
     dni = int(dni)
     edad = int(edad)
-    familia = Familiares(dni, edad, nombre)
+    familia = Familiares(dni=dni, edad=edad, nombre=nombre)
     familia.save()
-    return render (request, 'base.html', {'nombre': familia})
+    return render(request, 'base.html', {'nombre': familia})
+
 
 def inicio(request):
     return render( request, 'base1.html')
