2969540aaad839ae2c651042a5b37981f3a8765f
Bruno <102097102+BrunoMDonato@users.noreply.github.com>
2025-02-26 19:52:05 -0300

    Update urls.py

--- practicaMVT/urls.py
+++ practicaMVT/urls.py
@@ -1,23 +1,12 @@
-"""practicaMVT URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-import ent_MVT
+from django.shortcuts import redirect
+
+def redirigir_a_datos(request):
+    return redirect('inicio')  # Redirige a la vista llamada 'inicio' en ent_MVT/urls.py
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ent_MVT/', include('ent_MVT.urls')),
+    path('', redirigir_a_datos),  # Redirige la raíz a /datos/
 ]
